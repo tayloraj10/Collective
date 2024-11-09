891c8908de6f208339498b5d8175839b5c3d2019
tayloraj10 <taylor.alan.johnson@gmail.com>
2024-11-09 02:38:13 -0500

    add ability to submit images with initiative contribution

--- lib/components/initiatives_card.dart
+++ lib/components/initiatives_card.dart
@@ -2,11 +2,17 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:collective/constants.dart';
 import 'package:collective/models/app_data.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
 import 'package:percent_indicator/percent_indicator.dart';
 import 'package:provider/provider.dart';
+import 'package:file_picker/file_picker.dart';
+// import 'package:googleapis_auth/auth_browser.dart';
+// import 'package:collective/secrets.dart';
+// import 'package:googleapis/drive/v3.dart' as gdrive;
+// import 'package:googleapis_auth/auth_io.dart';
 
 class InitiativeCard extends StatefulWidget {
   final Map data;
@@ -20,6 +26,7 @@ class InitiativeCard extends StatefulWidget {
 class _InitiativeCardState extends State<InitiativeCard> {
   final TextEditingController contributionController = TextEditingController();
   int streak = 0;
+  List<PlatformFile> uploadedImages = [];
 
   @override
   void initState() {
@@ -148,7 +155,7 @@ class _InitiativeCardState extends State<InitiativeCard> {
   //         'userID': userData['uid'],
   //         'amount': int.tryParse(contributionController.text),
   //         'date': DateTime.now()
-  //       }
+  //       }mmm
   //     ])
   //   }).then(
   //       (value) => {
@@ -157,7 +164,7 @@ class _InitiativeCardState extends State<InitiativeCard> {
   //       onError: (e) => print("Error updating document $e"));
   // }
 
-  void addContribution(Map userData) {
+  void addContribution(Map userData) async {
     var ref = FirebaseFirestore.instance.collection("goal_submissions");
     ref.add({
       'userName': userData['name'],
@@ -165,10 +172,68 @@ class _InitiativeCardState extends State<InitiativeCard> {
       'initiative': this.widget.data['title'],
       'initiativeID': this.widget.data['id'],
       'amount': int.tryParse(contributionController.text),
-      'date': DateTime.now()
-    });
+      'date': DateTime.now(),
+    }).then(
+        (value) => {
+              if (uploadedImages.length > 0)
+                {
+                  uploadedImages.forEach((element) async {
+                    await uploadFile(element, value.id);
+                  })
+                }
+            },
+        onError: (e) => print("Error updating document $e"));
+
     runUpdates();
   }
+
+  Future<void> addFile(Function setState) async {
+    // authenticate();
+    final FilePickerResult? result = await FilePicker.platform
+        .pickFiles(type: FileType.image, allowMultiple: true);
+    if (result != null && result.files.isNotEmpty) {
+      // final file = File(result.files.single.path!);
+
+      setState(() {
+        uploadedImages = [...uploadedImages, ...result.files];
+      });
+    }
+  }
+
+  uploadFile(PlatformFile file, String docId) async {
+    FirebaseStorage storage = FirebaseStorage.instance;
+    Reference ref = storage.ref().child(
+        'initiatives/${DateTime.now().millisecondsSinceEpoch.toString() + "_" + file.name}');
+    UploadTask uploadTask = ref.putData(file.bytes!);
+
+    return await uploadTask.then((res) async {
+      String downloadUrl = await res.ref.getDownloadURL();
+      print("File uploaded successfully. Download URL: $downloadUrl");
+
+      FirebaseFirestore.instance
+          .collection('goal_submissions')
+          .doc(docId)
+          .update({
+        'images': FieldValue.arrayUnion([downloadUrl])
+      });
+    }).catchError((error) {
+      print("Error uploading file: $error");
+    });
+    // final driveFile = gdrive.File()..name = file.name;
+    // final media = gdrive.Media(Stream.fromIterable([file.bytes!]), file.size);
+    // await driveApi!.files.create(driveFile, uploadMedia: media);
+  }
+
+  // gdrive.DriveApi? driveApi;
+  // Future<void> authenticate() async {
+  //   final _scopes = [gdrive.DriveApi.driveFileScope];
+  //   final id = ClientId(client_id, null);
+  //   final authClient = await createImplicitBrowserFlow(id, _scopes)
+  //       .then((flow) => flow.clientViaUserConsent());
+  //   setState(() {
+  //     driveApi = gdrive.DriveApi(authClient);
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -193,54 +258,99 @@ class _InitiativeCardState extends State<InitiativeCard> {
           showDialog(
               context: context,
               builder: (BuildContext context) {
-                return StatefulBuilder(builder: (context, setState) {
-                  return AlertDialog(
-                      content: Container(
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Text("Make a Contribution",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 22)),
-                        TextField(
-                          controller: contributionController,
-                          keyboardType: TextInputType.number,
-                          inputFormatters: <TextInputFormatter>[
-                            FilteringTextInputFormatter.allow(RegExp(r'[0-9]')),
-                            FilteringTextInputFormatter.digitsOnly
-                          ],
-                          decoration: InputDecoration(
-                              labelText: 'Amount',
-                              errorText: checkInputError()
-                                  ? "Please enter a value that is 10 or less"
-                                  : null),
-                          textAlign: TextAlign.center,
-                          onChanged: (_) => setState(() {}),
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        ElevatedButton(
-                            onPressed: () => {
-                                  if (!checkInputError())
-                                    {
-                                      addContribution(userData),
-                                      // addContributionData(),
-                                      Navigator.pop(context)
-                                    }
-                                },
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Text(
-                                "Submit",
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold, fontSize: 16),
+                uploadedImages = [];
+                return AlertDialog(
+                  content: StatefulBuilder(
+                    builder: (BuildContext context, StateSetter setState) {
+                      return Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text("Make a Contribution",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 22)),
+                          TextField(
+                            controller: contributionController,
+                            keyboardType: TextInputType.number,
+                            inputFormatters: <TextInputFormatter>[
+                              FilteringTextInputFormatter.allow(
+                                  RegExp(r'[0-9]')),
+                              FilteringTextInputFormatter.digitsOnly
+                            ],
+                            decoration: InputDecoration(
+                                labelText: 'Amount',
+                                errorText: checkInputError()
+                                    ? "Please enter a value that is 10 or less"
+                                    : null),
+                            textAlign: TextAlign.center,
+                            onChanged: (_) => setState(() {}),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              ElevatedButton(
+                                  onPressed: () => {
+                                        if (!checkInputError() &&
+                                            contributionController
+                                                    .text.length !=
+                                                0)
+                                          {
+                                            addContribution(userData),
+                                            Navigator.pop(context)
+                                          }
+                                      },
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.green,
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text(
+                                      "Submit",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 16),
+                                    ),
+                                  )),
+                              SizedBox(
+                                width: 20,
                               ),
-                            ))
-                      ],
-                    ),
-                  ));
-                });
+                              ElevatedButton(
+                                  onPressed: () => {
+                                        addFile(setState),
+                                      },
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text(
+                                      "Add Image",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 16),
+                                    ),
+                                  )),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          if (uploadedImages.length > 0)
+                            SingleChildScrollView(
+                              scrollDirection: Axis.horizontal,
+                              child: Wrap(
+                                children: uploadedImages.map((file) {
+                                  return Image.memory(
+                                    Uint8List.fromList(file.bytes!),
+                                    height: 200,
+                                  );
+                                }).toList(),
+                              ),
+                            ),
+                        ],
+                      );
+                    },
+                  ),
+                );
               })
         },
         child: Card(
@@ -266,7 +376,7 @@ class _InitiativeCardState extends State<InitiativeCard> {
                 if (FirebaseAuth.instance.currentUser != null && streak > 0)
                   Padding(
                     padding: EdgeInsets.symmetric(horizontal: 15),
-                    child: Row(
+                    child: Wrap(
                       children: [
                         Text(
                           'You are on a ${streak} day Streak',
