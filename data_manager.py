9e0fe57535341f28c327a300225f2e945db67065
tayloraj10 <taylor.alan.johnson@gmail.com>
2025-01-11 19:44:53 -0500

    files related to photo uploads script

--- data_manager.py
+++ data_manager.py
@@ -1,18 +1,24 @@
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+import os
+from pydrive.drive import GoogleDrive
+from pydrive.auth import GoogleAuth
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+from firebase_admin import storage
 from pprint import pprint
 
-cred = credentials.Certificate(r"C:\Users\taylo\Downloads\collective-e06e1-firebase-adminsdk-2iy4z-ffbbbb44b6.json")
+cred = credentials.Certificate(
+    "collective-sa.json")
 app = firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 
-
+### DATA
 def view_locations_data():
     for x in db.collection(u'locations').get():
         pprint(x.to_dict())
-
 
 
 def create_new_location(name, address, description, glink, website, lat, lon):
@@ -37,3 +43,92 @@ create_new_location(
 )
 
 
+### PHOTOS
+import os
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from firebase_admin import storage
+from datetime import datetime, timedelta
+import pytz
+import shutil
+
+# If modifying these SCOPES, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/drive.file']
+SERVICE_ACCOUNT_FILE = "collective-sa.json"
+PHOTOS_SERVICE_ACCOUNT_FILE = "collective-photo-sa.json"
+PHOTOS_FOLDER_ID = '1VHRjAyYyW2PGv8OVhVX91laIghzic8ET'
+
+cred = credentials.Certificate(
+    "collective-sa.json")
+appF = firebase_admin.initialize_app(cred, 
+                                    {'storageBucket': 'collective-e06e1.appspot.com',})
+db = firestore.client()
+
+photo_cred = service_account.Credentials.from_service_account_file(
+    PHOTOS_SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+service = build('drive', 'v3', credentials=photo_cred)
+
+def upload_photo(file_path, file_name):
+    file_metadata = {'name': file_name, 'parents': [PHOTOS_FOLDER_ID]}
+    media = MediaFileUpload(file_path, mimetype='image/jpeg')
+    file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+    print(f"File ID: {file.get('id')}")
+    return file.get('id')
+
+def update_photo_url(id, fb_id, index):
+    updated = False
+    gdrive_url = 'https://drive.usercontent.google.com/download?id='
+    doc_ref = db.collection(u'goal_submissions').document(fb_id)
+    doc = doc_ref.get()
+    if doc.exists:
+        images = doc.to_dict().get('images', [])
+        if len(images) > index:
+            images[index] = gdrive_url + id
+        doc_ref.update({u'images': images})
+        updated = True
+        print(f"Updated document {doc.id} with image ID {id}")
+    return updated
+    
+
+def move_storage_files_to_gdrive():
+    folder_name = 'initiative_photos'
+    if os.path.exists(folder_name):
+            shutil.rmtree(folder_name)
+    bucket = storage.bucket(app=appF)
+    blobs = bucket.list_blobs(prefix='initiatives/')
+    tz = pytz.utc
+    one_week_ago = datetime.now(tz) - timedelta(weeks=1)
+    files = [blob for blob in blobs if blob.time_created < one_week_ago and blob.name != 'initiatives/']
+    
+    if len(files) > 0:
+        filename_counts = {}
+        for file in files:
+            filename = file.name.split('initiatives/', 1)[1]
+            file_path = os.path.join(folder_name, filename)
+            os.makedirs(os.path.dirname(file_path), exist_ok=True)
+            with open(file_path, 'wb') as f:
+                file.download_to_file(f)
+            print(f"Downloaded {filename}")
+            fb_id = filename.split('_', 1)[0]
+            if fb_id in filename_counts:
+                filename_counts[fb_id] += 1
+            else:
+                filename_counts[fb_id] = 0
+
+            id = upload_photo(file_path, filename)
+            if id:
+                update_photo_url(id, fb_id, filename_counts[fb_id])
+                file.delete()
+                # delete_blob = bucket.blob(file.name)
+                # delete_blob.delete()
+                print(f"Deleted {file.name} from Google Cloud Storage\n\n")
+
+        if os.path.exists(folder_name):
+            shutil.rmtree(folder_name)
+
+
+move_storage_files_to_gdrive()
